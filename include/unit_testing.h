ac9303521a5fcc190c04674366ac657b7c075893
Federico Galbiati <alfgalbiati@alice.it>
2018-11-13 22:38:30 -0500

    Added unit testing files

---
+++ include/unit_testing.h
@@ -0,0 +1,19 @@
+//
+//  unit_testing.h
+//  
+//
+//  Created by Federico Galbiati on 13/11/2018.
+//
+
+#ifndef unit_testing_h
+#define unit_testing_h
+
+#include <main.h>
+
+void beginUnitTesting();
+void clawTesting();
+void liftTesting();
+void driveTesting();
+void shooterTesting();
+
+#endif /* unit_testing_h */
---
+++ src/modules/unit_testing.c
@@ -0,0 +1,43 @@
+//
+//  unit_testing.c
+//  
+//
+//  Created by Federico Galbiati on 13/11/2018.
+//
+
+#include "unit_testing.h"
+#include "main.h"
+
+void beginUnitTesting() {
+    // Begin with lift testing
+    clawTesting();
+    liftTesting();
+    driveTesting();
+    shooterTesting();
+}
+
+void clawTesting() {
+    for (int i = 0; i < 30; i++) {
+        extendClaw();
+        retractClaw();
+    }
+}
+
+void liftTesting() {
+    for (int i = 0; i < 30; i++) {
+        liftOneLevelUp();
+        liftOneLevelUp();
+        liftOneLevelDown();
+        liftOneLevelDown();
+    }
+}
+
+void driveTesting() {
+    
+}
+
+void shooterTesting() {
+    for (int i = 0; i < 25; i++) {
+        shootBall();
+    }
+}
