46b1bc4aefdc7a8176c2fd9f10acbc64dbe8d44b
Federico Galbiati <alfgalbiati@alice.it>
2019-02-05 10:15:25 -0500

    Integrated new methods for tank assembly

--- V5-CPP/include/robotTankAssembly.h
+++ V5-CPP/include/robotTankAssembly.h
@@ -6,7 +6,10 @@ public:
     void moveBase(double speed, Direction direction);
     void moveLeftSide(double speed, Direction direction);
     void moveRightSide(double speed, Direction direction);
+    void moveLeftSide(double speed);
+    void moveRightSide(double speed);
     void moveByTile(double speed, Direction direction);
+    void rotateBaseOfAngle(double angle);
 };
 
 #endif
--- V5-CPP/src/robotTankAssembly.cpp
+++ V5-CPP/src/robotTankAssembly.cpp
@@ -1,17 +1,48 @@
 #include "robotTankAssembly.h"
 
+
+// Speed out of 200 (RPM). If direction == stop, stop even if speed != 0
 void RobotTankAssembly::moveBase(double speed, Direction direction) {
-    
+    moveLeftSide(speed, direction);
+    moveRightSide(speed, direction);
 }
 
 void RobotTankAssembly::moveLeftSide(double speed, Direction direction) {
-    
+    switch (direction) {
+        case stop: speed = 0;
+        case forward: speed = fabs(speed);
+        case backward: speed = -1 * fabs(speed);
+    }
+    baseMotorLeftFront.move_velocity(speed);
+    baseMotorLeftBack.move_velocity(speed);
 }
 
 void RobotTankAssembly::moveRightSide(double speed, Direction direction) {
+    switch (direction) {
+        case stop: speed = 0;
+        case forward: speed = fabs(speed);
+        case backward: speed = -1 * fabs(speed);
+    }
+    baseMotorRightFront.move_velocity(speed);
+    baseMotorRightBack.move_velocity(speed);
+}
+
+// Speed in range +- 127, used for controller analog
+void RobotTankAssembly::moveLeftSide(double speed) {
+    baseMotorLeftFront.move(speed);
+    baseMotorLeftBack.move(speed);
+}
+
+void RobotTankAssembly::moveRightSide(double speed) {
+    baseMotorRightFront.move(speed);
+    baseMotorRightBack.move(speed);
+}
+
+// Used by the autonomous function
+void RobotTankAssembly::moveByTile(double speed, Direction direction) {
     
 }
 
-void RobotTankAssembly::moveByTile(double speed, Direction direction) {
+void RobotTankAssembly::rotateBaseOfAngle(double angle) {
     
 }
