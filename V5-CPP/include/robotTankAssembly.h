c7ac29e0d9ef21e4d3de48b1653152c4064a30a4
Federico Galbiati <alfgalbiati@alice.it>
2019-02-05 10:19:42 -0500

    Updated .h file for tank assembly

--- V5-CPP/include/robotTankAssembly.h
+++ V5-CPP/include/robotTankAssembly.h
@@ -6,8 +6,12 @@ public:
     void moveBase(double speed, Direction direction);
     void moveLeftSide(double speed, Direction direction);
     void moveRightSide(double speed, Direction direction);
+    
+    // Speed values in rangee +- 127 for joysticks
     void moveLeftSide(double speed);
     void moveRightSide(double speed);
+    
+    // Mainly used by autonomous sequences
     void moveByTile(double speed, Direction direction);
     void rotateBaseOfAngle(double angle);
 };
