3ca55fff021502d7d67ee871efbdcaabf745e174
Niklex21 <alekseynikanov.21@gmail.com>
2019-03-13 14:39:25 -0400

    Cleaned up repo

--- OLD/include/ballLoader.h
+++
@@ -1,8 +0,0 @@
-#ifndef BALLLOADER_H_
-#define BALLLOADER_H_
-
-#include "main.h"
-
-void ballLoaderControl();
-
-#endif // BALLLOADER_H_
--- OLD/include/flipper.h
+++
@@ -1,8 +0,0 @@
-#ifndef FLIPPER_H_
-#define FLIPPER_H_
-
-#include "main.h"
-
-void flipperControl(int speed);
-
-#endif // FLIPPER_H_
